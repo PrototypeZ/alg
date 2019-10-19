15342743360eae1252abb3ab8c861499eac437b1
prototypez <prototypez@126.com>
2019-10-19 17:54:53 +0800

    update q31

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 public class Main {
+
+    /**
+     * 数组的几何意义：
+     * a 为数组， i,j 为数组中两个数的下标，i < j
+     *
+     * i - j 的意义： i 和 j 之间有几个数（包含 i 但是不包含 j 或者包含 j 但是不包含 i）
+     *
+     * (i + j) / 2 的意义： i 和 j 中间的那个数的下标， 例如下标为 0，3 中间的数下标为 1, 即第二个数
+     * 2, 4 中间的数的下标为 3 ，即第四个数。
+     *
+     *
+     * i - j + 1 的意义： i 和 j 之间有几个数，包括 i 和 j 本身
+     *
+     *
+     * 如果 i <= k <= j ， 如果要翻转 i ~ j 中间的所有的数，k 翻转后的位置在 j - (k - i) = j + i - k
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
--- src/leetcode/q31/Solution.java
+++ src/leetcode/q31/Solution.java
@@ -5,8 +5,38 @@ import java.util.Arrays;
 public class Solution {
 
     public void nextPermutation(int[] nums) {
+        int j = -1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1]) {
+                j = i;
+            }
+        }
+        if (j == -1) {
+            // j not found
+            Arrays.sort(nums);
+            return;
+        }
+        // j found
+        int k = j + 1;
+        for (int i = j + 2; i < nums.length; i++) {
+            if (nums[i] > nums[j]) {
+                k = i;
+            }
+        }
+        // exchange j and k
+        int tmp = nums[j];
+        nums[j] = nums[k];
+        nums[k] = tmp;
 
-
+        // reverse & exchange between j+1 ~ n (inclusive)
+        // j+1 .......i........nums.length - 1
+        // i  <--->  nums.length - 1 - (i-j-1)
+        // i  <--->  nums.length - i + j)
+        for (int i = j + 1; i <= (j + 1 + nums.length - 1) / 2; i++) {
+            int t = nums[i];
+            nums[i] = nums[nums.length - 1 - i + j + 1];
+            nums[nums.length - i + j] = t;
+        }
     }
 
     public static void main(String[] args) {
@@ -23,5 +53,14 @@ public class Solution {
         int[] test3 = new int[]{1, 1, 5};
         solution.nextPermutation(test3);
         System.out.println(Arrays.toString(test3));
+
+
+        int[] test4 = new int[]{1, 3, 2};
+        solution.nextPermutation(test4);
+        System.out.println(Arrays.toString(test4));
+
+        int[] test5 = new int[]{2, 3, 1};
+        solution.nextPermutation(test5);
+        System.out.println(Arrays.toString(test5));
     }
 }
