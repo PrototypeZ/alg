f0e30f7bed0ff2282fb9f2dadb61828857e8d345
prototypez <prototypez@126.com>
2021-03-11 10:07:53 +0800

    update semaphore

--- src/concurrent/semaphore/Solution.java
+++ src/concurrent/semaphore/Solution.java
@@ -7,6 +7,12 @@ import java.util.concurrent.Semaphore;
 public class Solution {
 
 
+    /**
+     * 要达到的效果，有 20 个线程，每个线程的任务都是打印一句话，然后退出。
+     * 但是同时只能有 5 个线程在执行任务，剩下的线程要等待别的线程执行完，自己才可以执行。
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         ExecutorService exec = Executors.newCachedThreadPool();
         // 只能5个线程同时访问
@@ -14,17 +20,16 @@ public class Solution {
         // 模拟20个客户端访问
         for (int index = 0; index < 20; index++) {
             final int NO = index;
-            Runnable run = new Runnable() {
-                public void run() {
-                    try {
-                        // 获取许可
-                        semp.acquire();
-                        System.out.println("Accessing: " + NO);
-                        Thread.sleep((long) (Math.random() * 10000));
-                        // 访问完后，释放 ，如果屏蔽下面的语句，则在控制台只能打印5条记录，之后线程一直阻塞
-                        semp.release();
-                    } catch (InterruptedException e) {
-                    }
+            Runnable run = () -> {
+                try {
+                    // 获取许可
+                    semp.acquire();
+                    System.out.println("Accessing: " + NO);
+                    Thread.sleep((long) (Math.random() * 10000));
+                    // 访问完后，释放 ，如果屏蔽下面的语句，则在控制台只能打印5条记录，之后线程一直阻塞
+                    semp.release();
+                    System.out.println("Existing: " + NO);
+                } catch (InterruptedException e) {
                 }
             };
             exec.execute(run);
