0c2fc3e85292709f2b0d0cbdd5484fa179b55f98
prototypez <prototypez@126.com>
2021-03-12 16:27:13 +0800

    update reverse

--- src/codingInterviews/reverse/Solution.java
+++ src/codingInterviews/reverse/Solution.java
@@ -4,6 +4,7 @@ import util.ListNode;
 import util.Util;
 
 /**
+ * 翻转一个单链表
  * Created by Jason on 2020/10/14/0014.
  */
 public class Solution {
@@ -14,7 +15,11 @@ public class Solution {
             return head;
         }
         ListNode newHead = reverse(head.next);
+        // 当前 head 指向还是原来的头指针
+        // 当前 head.next 还是指向原来头指针后面一个节点，但是这个节点现在已经是尾节点了
+        // 所以尾结点的 next 指向 head
         head.next.next = head;
+        // 当前的 head 已经成为尾结点了，所以 next 置为 null
         head.next = null;
         return newHead;
     }
