59a60b4156fff2bcd6a61b75b7be9ff77aa7c8a1
prototypez <prototypez@126.com>
2021-03-15 20:39:20 +0800

    update reconstructByPreAndMiddle

--- src/codingInterviews/tree/reconstructByPreAndMiddle/Solution.java
+++ src/codingInterviews/tree/reconstructByPreAndMiddle/Solution.java
@@ -5,7 +5,37 @@ import util.Util;
 
 /**
  *
- * ���ݶ�����ǰ����������������ؽ�������
+ * 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ *
+ *  
+ *
+ * 例如，给出
+ *
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 返回如下的二叉树：
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *  
+ *
+ * 限制：
+ *
+ * 0 <= 节点个数 <= 5000
+ *
+ *  
+ *
+ * 注意：本题与主站 105 题重复：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+ *
+ * 通过次数114,613提交次数165,777
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zhong-jian-er-cha-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 根据二叉树前序和中序遍历结果，重建二叉树
  * Created by Jason on 2020/10/15/0015.
  */
 public class Solution {
@@ -15,6 +45,16 @@ public class Solution {
         return parseInternal(preResult, 0, preResult.length - 1, middleResult, 0, middleResult.length - 1);
     }
 
+    /**
+     * 重建一棵子树
+     * @param preResult 整棵树前序遍历的结果
+     * @param preStart 需要重建的子树，前序遍历子序列在整棵树前序遍历的结果中起始位置
+     * @param preEnd 需要重建的子树，前序遍历子序列在整棵树前序遍历的结果中结束位置
+     * @param middleResult 整棵树中序遍历的结果
+     * @param middleStart 需要重建的子树，中序遍历子序列在整棵树中序遍历的结果中起始位置
+     * @param middleEnd 需要重建的子树，中序遍历子序列在整棵树中序遍历的结果中结束位置
+     * @return 重建好的子树的根节点
+     */
     public TreeNode parseInternal(int[] preResult, int preStart, int preEnd, int[] middleResult, int middleStart, int middleEnd) {
         int rootVal = preResult[preStart];
         TreeNode root = new TreeNode(preResult[preStart]);
